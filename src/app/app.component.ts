88ab6b6ac01903eac3f7af2496c7dee8492b4200
amsyafiiq <amsyafiiq@gmail.com>
2025-01-18 11:19:14 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mulah-simple-assessment';
+
+  table1Data = signal<TableData[]>([
+    { index: 'A1', value: 41 },
+    { index: 'A2', value: 18 },
+    { index: 'A3', value: 21 },
+    { index: 'A4', value: 63 },
+    { index: 'A5', value: 2 },
+    { index: 'A6', value: 53 },
+    { index: 'A7', value: 5 },
+    { index: 'A8', value: 57 },
+    { index: 'A9', value: 60 },
+    { index: 'A10', value: 93 },
+    { index: 'A11', value: 28 },
+    { index: 'A12', value: 3 },
+    { index: 'A13', value: 90 },
+    { index: 'A14', value: 39 },
+    { index: 'A15', value: 80 },
+    { index: 'A16', value: 88 },
+    { index: 'A17', value: 49 },
+    { index: 'A18', value: 60 },
+    { index: 'A19', value: 26 },
+    { index: 'A20', value: 28 },
+  ]);
+
+  table2Data = computed(() => {
+    return [
+      {
+        category: 'Alpha',
+        value:
+          (this.table1Data().find((data) => data.index === 'A5')?.value || 0) +
+          (this.table1Data().find((data) => data.index === 'A20')?.value || 0),
+      },
+      {
+        category: 'Beta',
+        value:
+          (this.table1Data().find((data) => data.index === 'A15')?.value || 0) /
+          (this.table1Data().find((data) => data.index === 'A7')?.value || 0),
+      },
+      {
+        category: 'Charlie',
+        value:
+          (this.table1Data().find((data) => data.index === 'A13')?.value || 0) *
+          (this.table1Data().find((data) => data.index === 'A12')?.value || 0),
+      },
+    ];
+  });
+
+  constructor() {}
+}
+
+export interface TableData {
+  index: string;
+  value: number;
 }
